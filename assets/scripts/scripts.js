b2944ba81c1ae36052e7f39979a87694ca48af55
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2025-12-24 17:32:05 +0530

    removed scripts.js

--- assets/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-function array(){
-    const input = document.getElementById("user_input_one").value;
-    const positionRaw = document.getElementById("user_input_two").value;
-    const replacementRaw = document.getElementById("user_input_three").value;
-    const errorMsg = document.getElementById("error_msg");
-
-    
-}
